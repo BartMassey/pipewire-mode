66402cd6e49ef4d96a9e6ca10515957a76c4db5f
Bart Massey <bart.massey@gmail.com>
2021-12-21 18:16:57 -0800

    added support for multiple commands

--- pipewire-mode.sh
+++ pipewire-mode.sh
@@ -1,27 +1,45 @@
 #!/bin/sh
+# Manage Pipewire (vs Pulseaudio) on a Debian box.
+# Bart Massey 2021-12
 
 SYSTEMCTL="systemctl --user --now"
-
-$SYSTEMCTL disable pipewire-media-session.service
-$SYSTEMCTL disable pipewire-pulse.service
-$SYSTEMCTL disable pipewire.service pipewire.socket
-$SYSTEMCTL disable pulseaudio.service pulseaudio.socket
-systemctl --user mask pulseaudio
-
-sudo apt -t unstable install \
-  pipewire wireplumber \
-  pipewire-media-session- pulseaudio-module-bluetooth- \
-  pipewire-audio-client-libraries pipewire-pulse \
-  libspa-0.2-jack libspa-0.2-bluetooth \
-  qjackctl
-
 EXAMPLES=/usr/share/doc/pipewire/examples
-sudo cp $EXAMPLES/alsa.conf.d/99-pipewire-default.conf /etc/alsa/conf.d/
-sudo cp $EXAMPLES/ld.so.conf.d/pipewire-jack-*.conf /etc/ld.so.conf.d/
-sudo ldconfig
 
-systemctl --user daemon-reload
-
-$SYSTEMCTL enable pipewire.service pipewire.socket
-$SYSTEMCTL enable pipewire-pulse.service
-$SYSTEMCTL enable wireplumber.service
+case "$1" in
+    install_pipewire)
+        cat <<EOF |
+        apt -t unstable install \
+          pipewire wireplumber \
+          pipewire-media-session- pulseaudio-module-bluetooth- \
+          pipewire-audio-client-libraries pipewire-pulse \
+          libspa-0.2-jack libspa-0.2-bluetooth \
+          qjackctl
+        systemctl --user daemon-reload
+        cp $EXAMPLES/alsa.conf.d/99-pipewire-default.conf /etc/alsa/conf.d/
+        cp $EXAMPLES/ld.so.conf.d/pipewire-jack-*.conf /etc/ld.so.conf.d/
+        ldconfig
+EOF
+        sudo sh
+        sh "$0" enable-pipewire
+        ;;
+    enable-pipewire)
+        sh "$0" disable-pulseaudio
+        $SYSTEMCTL enable pipewire.service pipewire.socket
+        $SYSTEMCTL enable pipewire-pulse.service
+        $SYSTEMCTL enable wireplumber.service
+        ;;
+    enable-pulseaudio)
+        sh "$0" disable-pipewire
+        $SYSTEMCTL unmask pulseaudio
+        $SYSTEMCTL enable pulseaudio.service pulseaudio.socket
+        ;;
+    disable-pipewire)
+        $SYSTEMCTL disable pipewire-media-session.service
+        $SYSTEMCTL disable pipewire-pulse.service
+        $SYSTEMCTL disable pipewire.service pipewire.socket
+        ;;
+    disable-pulseaudio)
+        $SYSTEMCTL disable pulseaudio.service pulseaudio.socket
+        $SYSTEMCTL mask pulseaudio
+        ;;
+esac
